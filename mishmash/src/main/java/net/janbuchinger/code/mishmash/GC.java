e1df8d92314619a90b487ae58eb46affacf97613
janbuchinger <janbuchinger@protonmail.ch>
2018-01-12 19:11:46 +0100

    revised doc

--- mishmash/src/main/java/net/janbuchinger/code/mishmash/GC.java
+++ mishmash/src/main/java/net/janbuchinger/code/mishmash/GC.java
@@ -16,7 +16,7 @@
 package net.janbuchinger.code.mishmash;
 
 /**
- * GC is an abbreviation for "german chars" and may be pretty unusable.
+ * GC is an abbreviation for &quot;german chars&quot; and may be pretty unusable.
  * <p>
  * Maybe it will be used as cheat sheet for unicode codes of special chars.
  * 
@@ -26,54 +26,54 @@ package net.janbuchinger.code.mishmash;
 public final class GC {
 
 	/**
-	 * "Umlaut A" as unicode character is "\u00C4".
+	 * &quot;Umlaut A&quot; as unicode character is &quot;\u00C4&quot;.
 	 * 
-	 * @return The unicode character <code>String</code> for "Umlaut A".
+	 * @return The unicode character <code>String</code> for &quot;Umlaut A&quot;.
 	 */
 	public final static String Ae() {
 		return "\u00C4";
 	}
 
 	/**
-	 * "Umlaut a" as unicode character is "\u00E4".
+	 * &quot;Umlaut a&quot; as unicode character is &quot;\u00E4&quot;.
 	 * 
-	 * @return The unicode character <code>String</code> for "Umlaut a".
+	 * @return The unicode character <code>String</code> for &quot;Umlaut a&quot;.
 	 */
 	public final static String ae() {
 		return "\u00E4";
 	}
 
 	/**
-	 * "Umlaut U" as unicode character is "\u00DC".
+	 * &quot;Umlaut U&quot; as unicode character is &quot;\u00DC&quot;.
 	 * 
-	 * @return The unicode character <code>String</code> for "Umlaut U".
+	 * @return The unicode character <code>String</code> for &quot;Umlaut U&quot;.
 	 */
 	public final static String Ue() {
 		return "\u00DC";
 	}
 
 	/**
-	 * "Umlaut u" as unicode character is "\u00FC".
+	 * &quot;Umlaut u&quot; as unicode character is &quot;\u00FC&quot;.
 	 * 
-	 * @return The unicode character <code>String</code> for "Umlaut u".
+	 * @return The unicode character <code>String</code> for &quot;Umlaut u&quot;.
 	 */
 	public final static String ue() {
 		return "\u00FC";
 	}
 
 	/**
-	 * "Umlaut O" as unicode character is "\u00D6".
+	 * &quot;Umlaut O&quot; as unicode character is &quot;\u00D6&quot;.
 	 * 
-	 * @return The unicode character <code>String</code> for "Umlaut O".
+	 * @return The unicode character <code>String</code> for &quot;Umlaut O&quot;.
 	 */
 	public final static String Oe() {
 		return "\u00D6";
 	}
 
 	/**
-	 * "Umlaut o" as unicode character is "\u00F6".
+	 * &quot;Umlaut o&quot; as unicode character is &quot;\u00F6&quot;.
 	 * 
-	 * @return The unicode character <code>String</code> for "Umlaut o".
+	 * @return The unicode character <code>String</code> for &quot;Umlaut o&quot;.
 	 */
 	public final static String oe() {
 		return "\u00F6";
